83f91831b028f930a9176302f67d2bd756dc43e6
Brandon Desktop <linton97v@gmail.com>
2020-01-27 15:50:09 -0800

    Added Athlete and Meeting controlles with scaffolded views

---
+++ Milestones/Milestone_4/Sprint_2_Class_Assignment/SwimMeetTracker/Controllers/AthletesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SwimMeetTracker.DAL;
+using SwimMeetTracker.Models;
+
+namespace SwimMeetTracker.Controllers
+{
+    public class AthletesController : Controller
+    {
+        private TrackMeetContext db = new TrackMeetContext();
+
+        // GET: Athletes
+        public ActionResult Index()
+        {
+            return View(db.Athletes.ToList());
+        }
+
+        // GET: Athletes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // GET: Athletes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Athletes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AID,FirstName,LastName,DateOfBirth")] Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Athletes.Add(athlete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(athlete);
+        }
+
+        // GET: Athletes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // POST: Athletes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AID,FirstName,LastName,DateOfBirth")] Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(athlete).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(athlete);
+        }
+
+        // GET: Athletes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // POST: Athletes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Athlete athlete = db.Athletes.Find(id);
+            db.Athletes.Remove(athlete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
