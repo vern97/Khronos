195638c541c567b1daa1c60fce542a042d8dbf78
Brandon Desktop <linton97v@gmail.com>
2020-01-27 16:10:36 -0800

    Added example seed script to test athletes

---
+++ Milestones/Milestone_4/Sprint_2_Class_Assignment/SwimMeetTracker/App_Data/seed.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO [dbo].[Athletes] (FirstName, LastName, DateOfBirth) VALUES
+	('Brandon', 'Linton', '1/24/97'),
+	('Julia', 'Webster', '8/15/98')
+GO
+
+INSERT INTO [dbo].[Coaches] (FirstName, LastName) VALUES
+	('Max', 'Stoyanov'),
+	('Victoria', 'Rhine')
+Go
+
+INSERT INTO [dbo].[Teams] (CoachID, Name) VALUES
+	(1, 'Wolves'),
+	(2, 'Huskies')
+GO
