c8a61a9ffdbbbc7a34218b4a8f0c11866d18fb72
Brandon Linton <linton97v@gmail.com>
2020-01-29 10:20:13 -0800

    Revert "Please read quickly if confused"

--- Milestones/Milestone_3/Sprint_1_Class_Assignment/SwimMeetTracker/DAL/IdentityModels.cs
+++ Milestones/Milestone_3/Sprint_1_Class_Assignment/SwimMeetTracker/DAL/IdentityModels.cs
@@ -22,7 +22,8 @@
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("TrackMeetContext", throwIfV1Schema: false)
+            : base("TrackMeetContext_Azure", throwIfV1Schema: false)
+//            : base("AzureConnection", throwIf1Schema: false)
         {
             // Disable code-first migrations
             Database.SetInitializer<ApplicationDbContext>(null);
