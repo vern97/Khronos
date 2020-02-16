d3b451f30651177536d3c9b9bac5135146e2b660
Victoria-Rhine <vrhine18@mail.wou.edu>
2020-02-16 14:09:21 -0800

    Add drop in chat feature

--- BTT/BeyondTheTutor/BeyondTheTutor/Scripts/main.js
+++ BTT/BeyondTheTutor/BeyondTheTutor/Scripts/main.js
@@ -1,3 +1,10 @@
 ﻿$('#demo').datetimepicker({
     inline: true,
 });
+
+$('.message .close')
+    .on('click', function () {
+        $(this)
+            .closest('.message')
+            .transition('fade');
+});
