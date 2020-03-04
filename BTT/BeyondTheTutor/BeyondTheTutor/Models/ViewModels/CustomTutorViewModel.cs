d186c120f9790e14ddabe4aee0299aa163ce20b0
MaxWou <mstoyanov18@mail.wou.edu>
2020-03-03 18:52:43 -0800

    testing this change.

---
+++ BTT/BeyondTheTutor/BeyondTheTutor/Models/ViewModels/CustomTutorViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BeyondTheTutor.Models.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class CustomTutorViewModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string vNumber { get; set; }
+        public bool adminApproved { get; set; }
+        public string role { get; set; }
+    }
+}
