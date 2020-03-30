a4d0bbae0953dd7ff8a5ba464006525836c26f03
Victoria-Rhine <vrhine18@mail.wou.edu>
2020-03-30 13:13:25 -0700

    Add admin home banner

--- BTT/BeyondTheTutor/BeyondTheTutor/Areas/Admin/Controllers/HomeController.cs
+++ BTT/BeyondTheTutor/BeyondTheTutor/Areas/Admin/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using BeyondTheTutor.DAL;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace BeyondTheTutor.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
+        private BeyondTheTutorContext db = new BeyondTheTutorContext();
+
         // GET: Admin/Home
         public ActionResult Index()
         {
             ViewBag.Current = "AdminHomeIndex";
+
+            var userID = User.Identity.GetUserId();
+            var currentUser = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().FirstName;
+            ViewBag.User = currentUser;
+
             return View();
         }
 
