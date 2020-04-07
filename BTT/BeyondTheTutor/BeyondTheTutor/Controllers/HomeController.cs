cfbf3a8d3b5ea3fc97ff168e753cf8f886fcf3bb
Victoria-Rhine <vrhine18@mail.wou.edu>
2020-04-07 10:07:51 -0700

    Small change to method

--- BTT/BeyondTheTutor/BeyondTheTutor/Controllers/HomeController.cs
+++ BTT/BeyondTheTutor/BeyondTheTutor/Controllers/HomeController.cs
@@ -93,7 +93,7 @@ namespace BeyondTheTutor.Controllers
 
                 for (int i = 0; i < grades.Count(); i++)
                 {
-                    total += grades[i] * weights[i];
+                    total = total +  (grades[i] * weights[i]);
                 }
 
                 double firstNumber = total;
