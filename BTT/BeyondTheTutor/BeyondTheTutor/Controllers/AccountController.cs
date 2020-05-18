28550fdc744b7cc9a26b0898755e25f721221f79
MaxWou <mstoyanov18@mail.wou.edu>
2020-05-17 19:29:30 -0700

    Added a Recap to forgot password page.

--- BTT/BeyondTheTutor/BeyondTheTutor/Controllers/AccountController.cs
+++ BTT/BeyondTheTutor/BeyondTheTutor/Controllers/AccountController.cs
@@ -479,6 +479,8 @@ namespace BeyondTheTutor.Controllers
         [AllowAnonymous]
         public ActionResult ForgotPassword()
         {
+            ViewBag.ReCapKey = System.Web.Configuration.WebConfigurationManager.AppSettings["ReCapKey"];
+
             return View();
         }
 
@@ -488,7 +490,54 @@ namespace BeyondTheTutor.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> ForgotPassword(ForgotPasswordViewModel model)
         {
-            if (ModelState.IsValid)
+            bool _error = false;
+            var response = Request["g-recaptcha-response"];
+            //secret that was generated in key value pair
+            string secret = System.Web.Configuration.WebConfigurationManager.AppSettings["ReCapSecretKey"];
+
+            var client = new WebClient();
+            var reply =
+                client.DownloadString(
+                    string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
+
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
+
+            //when response is false check for the error message
+            if (captchaResponse.Success.Equals(false))
+            {
+                if (captchaResponse.ErrorCodes.Count <= 0) return View();
+
+                var error = captchaResponse.ErrorCodes[0].ToLower();
+                switch (error)
+                {
+                    case ("missing-input-secret"):
+                        ViewBag.Message = "The secret parameter is missing.";
+                        break;
+                    case ("invalid-input-secret"):
+                        ViewBag.Message = "The secret parameter is invalid or malformed.";
+                        break;
+
+                    case ("missing-input-response"):
+                        ViewBag.Message = "The response parameter is missing.";
+                        break;
+                    case ("invalid-input-response"):
+                        ViewBag.Message = "The response parameter is invalid or malformed.";
+                        break;
+
+                    default:
+                        ViewBag.Message = "Error occured. Please try again";
+                        break;
+                }
+
+                _error = true;
+            }
+            else
+            {
+                ViewBag.Message = "Valid";
+            }
+
+
+            if (ModelState.IsValid && !_error)
             {
 
                 var user = await UserManager.FindByNameAsync(model.Email);
@@ -512,6 +561,8 @@ namespace BeyondTheTutor.Controllers
 
                 return RedirectToAction("Index", "Home");
             }
+
+            ViewBag.ReCapKey = System.Web.Configuration.WebConfigurationManager.AppSettings["ReCapKey"];
 
             // If we got this far, something failed, redisplay form
             return View(model);
