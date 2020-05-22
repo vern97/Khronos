819e172fc8812e550df9d4d0a3bd2ae773a3c0aa
Brandon Desktop <linton97v@gmail.com>
2020-05-22 10:59:02 -0700

    Add student profile pictures

--- BTT/BeyondTheTutor/BeyondTheTutor/Controllers/StudentsController.cs
+++ BTT/BeyondTheTutor/BeyondTheTutor/Controllers/StudentsController.cs
@@ -4,6 +4,9 @@ using System.Web.Mvc;
 using BeyondTheTutor.DAL;
 using BeyondTheTutor.Models;
 using Microsoft.AspNet.Identity;
+using BeyondTheTutor.Models.ProfilePictureModels;
+using System.IO;
+using System.Web;
 
 namespace BeyondTheTutor.Controllers
 {
@@ -17,12 +20,21 @@ namespace BeyondTheTutor.Controllers
             ViewBag.Current = "StuProfile";
 
             var userID = User.Identity.GetUserId();
-            ViewBag.currentUserID = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().ID;
+            var currentUserID = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().ID;
+
+            ViewBag.currentUserID = currentUserID;
 
             ViewBag.userFirstName = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().FirstName;
             ViewBag.userLastName = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().LastName;
             ViewBag.gradYear = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().Student.GraduatingYear;
             ViewBag.classStand = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().Student.ClassStanding;
+
+            ViewBag.StuGradYear = db.Students.Where(m => m.ID == currentUserID).Select(m => m.GraduatingYear).FirstOrDefault().ToString();
+            ViewBag.StuClassStanding = db.Students.Where(m => m.ID == currentUserID).Select(m => m.ClassStanding).FirstOrDefault().ToString();
+
+            ViewBag.UserHasProfilePicture = db.ProfilePictures.Any(m => m.UserID == currentUserID);
+
+            ViewBag.ImagePath = null;
 
             return View();
         }
@@ -31,6 +43,7 @@ namespace BeyondTheTutor.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,GraduatingYear,ClassStanding")] Student student)
         {
+            
             if (ModelState.IsValid)
             {
                 db.Entry(student).State = EntityState.Modified;
@@ -39,6 +52,59 @@ namespace BeyondTheTutor.Controllers
             }
             ViewBag.ID = new SelectList(db.BTTUsers, "ID", "FirstName", student.ID);
             return View(student);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult StudentProfilePicture(HttpPostedFileBase userPicture)
+        {
+            
+
+            byte[] bytes;
+
+            var userID = User.Identity.GetUserId();
+            var currentUserID = db.BTTUsers.Where(m => m.ASPNetIdentityID.Equals(userID)).FirstOrDefault().ID;
+
+            
+
+            // Check if a user already has a current picture
+            var userHasPicture = db.ProfilePictures.Any(m => m.UserID == currentUserID);
+
+
+            if (userPicture != null)
+            {
+                if (userHasPicture == true)
+                {
+                    // Get ID of current picture
+                    var currentUserPicture = db.ProfilePictures.Where(m => m.UserID == currentUserID).FirstOrDefault().ID;
+                    // Get object of current picture
+                    ProfilePicture oldProfilePicture = db.ProfilePictures.Find(currentUserPicture);
+                    // Remove current picture to free space in db
+                    db.ProfilePictures.Remove(oldProfilePicture);
+                }
+
+                using (BinaryReader br = new BinaryReader(userPicture.InputStream))
+                {
+                    bytes = br.ReadBytes(userPicture.ContentLength);
+                }
+
+                db.ProfilePictures.Add(new ProfilePicture
+                {
+                    ImagePath = bytes,
+                    UserID = currentUserID
+                });
+
+                db.SaveChanges();
+
+            }
+
+            return RedirectToAction("StudentProfile");
+        }
+
+        public ActionResult RetrieveCurrentStudentProfilePicture(int id)
+        {
+            var profilePicture = db.ProfilePictures.Where(m => m.UserID == id).Select(m => m.ImagePath).FirstOrDefault();
+            return File(profilePicture, "image/jpg");
         }
 
         protected override void Dispose(bool disposing)
---
+++ BTT/BeyondTheTutor/BeyondTheTutor/Scripts/displaystudent.js
@@ -0,0 +1,34 @@
+﻿$('#document').ready(function () {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'Student/Home/GetStudent',
+        success: displayTutors,
+        error: errorOnAjax
+    });
+});
+
+function errorOnAjax() {
+    console.log('Error on AJAX return');
+}
+
+function displayTutors(data) {
+
+    $('#student_card').append(`
+        <div class="ui card portal">
+            <div class="ui image">
+                <img src="/Home/RetrieveCurrentTutorProfilePicture/${data[0].profilePictureID}" alt="Alternate Text" />
+            </div>
+            <div class="content">
+                <div class="ui centered dividing header"><p id="portal-headers-black">${data[0].FullName}</p></div>
+                <div class="description">
+                    <center>
+                        <span class="date">${data[0].ClassStanding}</span><br />
+                        <span class="date">Class of ${data[0].GradYear}</span>
+                    </center>
+                </div>
+            </div>
+        </div>
+    `)
+
+}
