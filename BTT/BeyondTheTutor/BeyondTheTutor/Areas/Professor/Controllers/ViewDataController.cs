4de9de7fe9fc896485adeee19a38abcab849749a
MaxWou <mstoyanov18@mail.wou.edu>
2020-05-31 08:05:38 -0700

    fixed my js, I THINK. This is all I got. otherwise, I have zero clue what was breaking the charts.

--- BTT/BeyondTheTutor/BeyondTheTutor/Areas/Professor/Controllers/ViewDataController.cs
+++ BTT/BeyondTheTutor/BeyondTheTutor/Areas/Professor/Controllers/ViewDataController.cs
@@ -22,28 +22,40 @@ namespace BeyondTheTutor.Areas.Professor.Controllers
             viewData.Students = db.Students.ToList();
             viewData.TimeSheets = db.TimeSheets.ToList();
 
-            ViewBag.seniors = db.Students.Where(m => m.ClassStanding == "Senior").Count();
-            ViewBag.juniors = db.Students.Where(m => m.ClassStanding == "Junior").Count();
-            ViewBag.sophmores = db.Students.Where(m => m.ClassStanding == "Sophmore").Count();
-            ViewBag.freshmen = db.Students.Where(m => m.ClassStanding == "Freshman").Count();
+            return View(viewData);
+        }
 
-            var DAY = DateTime.Now;
-            var day = DateTime.Now;
+        public JsonResult getBars()
+        {
+            List<object> custList = new List<object>();
 
-            foreach (var c in db.Classes.OrderBy(m => m.Name))
+
+
+            foreach (var c in db.Classes.OrderBy(m => m.Name).Distinct())
             {
-                string name = c.Name;
-                int count = db.TutoringAppts.Where(ta => ta.Class.Name == c.Name).Count();
-                if(count >= 1)
+                int noAppts = db.TutoringAppts.Where(ta => ta.Class.Name == c.Name).Count();
+                if (noAppts >= 1)
                 {
-                    viewData.courseAptFreq.Add(name, count);
-
+                    object data = new
+                    {
+                        name = c.Name,
+                        count = noAppts
+                    };
+                    custList.Add(data);
                 }
             }
+           
+            return Json(custList, JsonRequestBehavior.AllowGet);
+        }
 
+        public JsonResult getLines()
+        {
+            List<object> custList = new List<object>();
+
+            var day = DateTime.Now;// start at current day(month)
             List<DateTime> months = new List<DateTime>();
-            day = day.AddMonths(-12);
-            for(int i = 0; i < 12; i++) //made a list of months
+            day = day.AddMonths(-12);//go back 12 months so we have a graph from THEN until NOW
+            for (int i = 0; i < 12; i++) //made a list of months
             {
                 day = day.AddMonths(1);
                 months.Add(day);
@@ -54,17 +66,73 @@ namespace BeyondTheTutor.Areas.Professor.Controllers
             var appts = db.TutoringAppts.ToList();
 
             for (int i = 0; i < 12; i++)// iterate thru 12 months
-            { 
+            {
                 var count = appts// save the no. of sessions per month
-                    .Where(ta => 
+                    .Where(ta =>
                     ta.StartTime.Year == months[j].Year &&
                     ta.StartTime.Month == months[j].Month
                 ).Count();
-                viewData.courseAptFreq_dated.Add(months[j].ToString("MMMM yyyy"), count);//add data to dictionary
-                j++;//inc month
+
+                object data = new//we let zero counts go thru because we want 
+                {
+                    name = months[j++].ToString("MMMM yyyy"),
+                    count = count
+                };
+
+                custList.Add(data);
+
+                //j++;//inc month
             }
 
-            return View(viewData);
+
+            return Json(custList, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult getStu()
+        {
+            List<object> students = new List<object>();
+
+            var seniors = db.Students.Where(m => m.ClassStanding == "Senior").Count();
+            var juniors = db.Students.Where(m => m.ClassStanding == "Junior").Count();
+            var sophmores = db.Students.Where(m => m.ClassStanding == "Sophmore").Count();
+            var freshmen = db.Students.Where(m => m.ClassStanding == "Freshman").Count();
+
+            object data = new
+            {
+                name = seniors + " Seniors",
+                count = seniors
+            };
+
+            students.Add(data);
+
+            object data1 = new
+            {
+                name = juniors + " Juniors",
+                count = juniors
+            };
+
+            students.Add(data1);
+
+            object data2 = new
+            {
+                name = sophmores + " Sophmores",
+                count = sophmores
+            };
+
+            students.Add(data2);
+
+            object data3 = new
+            {
+                name = freshmen + " Freshmen",
+                count = freshmen
+            };
+
+            students.Add(data3);
+
+
+
+            return Json(students, JsonRequestBehavior.AllowGet);
         }
     }
 }
